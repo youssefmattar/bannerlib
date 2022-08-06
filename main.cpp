fe3b6da25e0f3b66688bf8b5453188fe9ff51e29
youssefmattar <youssefmattar2020@gmail.com>
2022-08-06 19:16:06 +0200

    bug fix

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include"bannerLib.h"
 //#include <windows.h>	//you need to uncomment this line if you want color output
+#include <conio.h>
 #define MAX_Buff_SZ 51
 //HANDLE  hConsole;    // this too :) 
 char* cmdBuff;
@@ -23,7 +24,7 @@ int main(int argc, char* argv[])
 		}
 		cmdBuff = (char*)malloc(sumc * sizeof(char));
 		int ii;
-		for (int ii = 1; ii < argc; ii++)
+		for (ii = 1; ii < argc; ii++)
 		{
 			if (ii == 1)
 			{
@@ -42,20 +43,29 @@ int main(int argc, char* argv[])
 		printBanner(cmdBuff);
 		//printf("%d", sumc);
 		free(cmdBuff);
+
+		printf("\npress any key to exit\n");
+		getch();
+		return(0);
 	}
 	else  //if there are no args it will ask the user for input
 	{ 
 		char* sBuff = (char*)malloc(MAX_Buff_SZ);
 		if (sBuff == NULL) {
 			printf("No memory\n");
-			return 1;
+			return(1);
 		}
 		printf("enter text you want to print max 50 characters\n");
 		fgets(sBuff, 50, stdin);
-		printBanner(sBuff); 
+		printBanner(sBuff);
+		//Sleep(1000);
+		free(sBuff);
+		printf("\npress any key to exit\n");
+		getch();
+		return(0);
 	}
 	//SetConsoleTextAttribute(hConsole, 15);  //optional if you want to set the color white again
-	return(0);
+	
 }
 
 
